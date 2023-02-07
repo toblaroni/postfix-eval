3ea76a8586c06c04c2960b2ab6ab1d752de86c47
toby hutchinson <tobyhutchinson@gmail.com>
2023-02-07 22:16:51 +0000

    Skeleton of main created

--- main.c
+++ main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include "stack.h"
+#include <string.h>
 
-int main(void) {
-    printf("hello");
+int main(int argc, char **argv) {
+    if (argc > 2) return -1;
+
+
+    // Get the expression from the input
+    char *expression = argv[1];
+    unsigned long len = strlen(expression);
+
+    // Make and initialise stack to the size of the expression
     Stack s;
-    initStack(&s, 50);
+    initStack(&s, len);
+
+    for (int i = 0; i < len; i++) {
+        switch (expression[i]) {
+            case '+':
+                printf("Plus sign\n");
+                break;
+            case '-':
+                printf("Minus Sign\n");
+                break;
+            case '*':
+                printf("Multiply Sign\n");
+                break;
+            case '/':
+                printf("Divide Sign\n");
+                break;
+            default:
+                printf("Other\n");
+        }
+    }
+
+    return 0;
 }
