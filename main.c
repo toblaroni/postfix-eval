f83504c3656baa17903a8b5d18b60c894edf8699
toby hutchinson <tobyhutchinson@gmail.com>
2023-02-08 00:03:00 +0000

    Implemented postfix computing with stack. Need to add floats so that division is possible

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 #include "stack.h"
 #include <string.h>
 
+int dPop(Stack *s, int *n, int *m);
+
 int main(int argc, char **argv) {
     if (argc > 2) return -1;
-
 
     // Get the expression from the input
     char *expression = argv[1];
@@ -22,29 +23,59 @@ int main(int argc, char **argv) {
             case '+': 
             {
                 int val1, val2; // values to hold the popped numbers
-                // Pop the stack twice
-                int popOne = pop(s, &val1);
-                int popTwo = pop(s, &val2);
 
-                if (popOne == 0 || popTwo == 0) {
-                    printf("Incorrect number of operands\n");
-                    return 1;
-                }
-                
+                // Check Operands are correct, pop the stack and assign to val1 and val2
+                if (!dPop(s, &val1, &val2)) return -1;
+
                 // Add numbers together and add result to stack
+                printf("Adding numbers %i and %i together\n", val1, val2);
                 val1 += val2;
                 push(s, val1);
+
                 break;
             }
             case '-':
-                printf("Minus Sign\n");
+            {
+                int val1, val2; // values to hold the popped numbers
+
+                // Check Operands are correct, pop the stack and assign to val1 and val2
+                if (!dPop(s, &val1, &val2)) return -1;
+
+                // Add numbers together and add result to stack
+                printf("Subtracting %i from %i\n", val1, val2);
+                val2 -= val1;
+                push(s, val2);
+
                 break;
+            }
             case '*':
-                printf("Multiply Sign\n");
+            {
+                int val1, val2; // values to hold the popped numbers
+
+                // Check Operands are correct, pop the stack and assign to val1 and val2
+                if (!dPop(s, &val1, &val2)) return -1;
+
+                // Add numbers together and add result to stack
+                printf("Multiplying numbers %i and %i together\n", val1, val2);
+                val1 *= val2;
+                push(s, val1);
+
                 break;
+            }
             case '/':
-                printf("Divide Sign\n");
+            {
+                int val1, val2; // values to hold the popped numbers
+
+                // Check Operands are correct, pop the stack and assign to val1 and val2
+                if (!dPop(s, &val1, &val2)) return -1;
+
+                // Add numbers together and add result to stack
+                printf("Dividing %i by %i\n", val2, val1);
+                val2 /= val1;
+                push(s, val2);
+
                 break;
+            }
             default:
             {
                 // Check it's a digit
@@ -65,4 +96,17 @@ int main(int argc, char **argv) {
     printf("Final sum = %i\n", result);
 
     return 0;
+}
+
+// Checks two values 
+int dPop(Stack *s, int *val1, int *val2) {
+    // Pop the stack twice
+    int popOne = pop(s, val1);
+    int popTwo = pop(s, val2);
+
+    if (popOne == 0 || popTwo == 0) {
+        printf("Incorrect number of operands\n");
+        return 0;
+    }
+    return 1;
 }
